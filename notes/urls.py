7fa71ec5ab69acaa4f987f3102e13d2240087d59
kananovichvitali <kananovich.vitali@gmail.com>
2026-03-31 18:07:22 +0200

    Added HTML template + CSS styles + sample data from our lesson

--- notes/urls.py
+++ notes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hello_from_notes_app
+from .views import hello_from_notes_app, show_product_list
 
 urlpatterns = [
     path('hello/', hello_from_notes_app),
+    path('show_product_list/', show_product_list),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,25 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def hello_from_notes_app(request):
     return HttpResponse("Hello from Notes app")
+
+def show_product_list(request):
+    products = [
+        {
+            "name": "Banana",
+            "price": 10,
+            "description": "fruit"
+        },
+        {
+            "name": "Apple",
+            "price": 8,
+            "description": "fruit"
+        },
+        {
+            "name": "Carrot",
+            "price": 2,
+            "description": "vegetable"
+        }
+    ]
+    return render(request, 'index.html', {'products': products})
