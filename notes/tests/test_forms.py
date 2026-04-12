600e58f6d7a81ae0676919e51904d502c9361e65
kananovichvitali <kananovich.vitali@gmail.com>
2026-04-12 20:06:41 +0200

    Remade tests (CRUD done)

--- notes/tests/test_forms.py
+++ notes/tests/test_forms.py
@@ -6,19 +6,44 @@ from .factories import CategoryFactory
 def test_note_form_valid():
     category = CategoryFactory()
     form_data = {
-        'title': 'Купити молоко',
-        'text': 'Треба купити 2 літри молока',
+        'title': 'Купити продукти',
+        'text': 'Молоко, хліб, сир',
         'category': category.id
     }
     form = NoteForm(data=form_data)
-    assert form.is_valid(), form.errors
+    assert form.is_valid()
+
 
 @pytest.mark.django_db
-def test_note_form_with_invalid_empty_title():
+def test_note_form_invalid_empty_fields():
+    form = NoteForm(data={'title': '', 'text': ''})
+    assert not form.is_valid()
+    assert 'title' in form.errors
+    assert 'text' in form.errors
+
+
+@pytest.mark.django_db
+def test_note_form_title_max_length():
+    category = CategoryFactory()
+    long_title = "A" * 101
     form_data = {
-        'title': '',
-        'text': 'Якийсь прикладовий текст'
+        'title': long_title,
+        'text': 'Тест довгого заголовку',
+        'category': category.id
     }
     form = NoteForm(data=form_data)
     assert not form.is_valid()
     assert 'title' in form.errors
+
+
+@pytest.mark.django_db
+def test_note_form_optional_fields():
+    category = CategoryFactory()
+    form_data = {
+        'title': 'Замітка без дати',
+        'text': 'Звичайний текст',
+        'category': category.id,
+        'reminder': ''
+    }
+    form = NoteForm(data=form_data)
+    assert form.is_valid()
--- notes/tests/test_models.py
+++ notes/tests/test_models.py
@@ -1,50 +1,48 @@
 import pytest
-from .factories import NoteFactory, UserFactory, CategoryFactory
+from .factories import NoteFactory, CategoryFactory, UserFactory
 from ..models import Note, Category
 
 
 @pytest.mark.django_db
-def test_category_creation():
-    category = CategoryFactory(title="Терміново")
-
+def test_category_crud():
+    category = CategoryFactory(title="Навчання")
     assert category.id is not None
-    assert category.title == "Терміново"
-    assert str(category) == "Терміново"
+    assert str(category) == "Навчання"
+
+    category.title = "Університет"
+    category.save()
+    assert Category.objects.get(id=category.id).title == "Університет"
+
+    category_id = category.id
+    category.delete()
+    assert not Category.objects.filter(id=category_id).exists()
 
 
 @pytest.mark.django_db
-def test_note_creation():
-    note = NoteFactory(title="Тестова нотатка 1")
-
+def test_note_crud_and_links(user, category):
+    note = NoteFactory(
+        author=user,
+        category=category,
+        title="Здати дз по Django",
+        text="Написати тести для CRUD"
+    )
     assert note.id is not None
-    assert note.title == "Тестова нотатка 1"
-    assert note.category is not None
 
+    assert note.get_absolute_url() == f"/{note.pk}/"
+    assert note.author.username == user.username
 
-@pytest.mark.django_db
-def test_note_update():
-    note = NoteFactory(title="Стара назва нотатки")
-    note.title = "Нова назва нотатки"
+    note.title = "Здати дз вчасно!"
     note.save()
+    assert Note.objects.get(id=note.id).title == "Здати дз вчасно!"
 
-    assert Note.objects.get(id=note.id).title == "Нова назва нотатки"
-
-
-@pytest.mark.django_db
-def test_note_author_relationship():
-    user = UserFactory(username="Ivan")
-    note = NoteFactory(author=user)
-
-    assert note.author.username == "Ivan"
-    assert user.notes.count() == 1
+    note_id = note.id
+    note.delete()
+    assert not Note.objects.filter(id=note_id).exists()
 
 
 @pytest.mark.django_db
 def test_note_cascade_delete_with_user(user, category):
-    NoteFactory(author=user, category=category, title="Секретна нотатка")
-
-    assert Note.objects.filter(author=user).count() == 1
-
+    NoteFactory(author=user, category=category)
     user_id = user.id
     user.delete()
     assert Note.objects.filter(author_id=user_id).count() == 0
